79d9d6566daf1169e14c5eeac85ca29e753b3337
lriman <knight.max.jd@gmail.com>
2018-07-03 10:22:56 +0300

    stash

--- ether/proxy.js
+++ ether/proxy.js
@@ -4,9 +4,10 @@
 
 const Web3 = require('web3');
 
-const config = require('../config/config');
+const config = require('../config/config'),
+      logger = require('../utils/logger')(module);
 
-let ethClients = [], // reference to clients
+let ethClients = [],
     lastBlock = 0,
     c = config.color,
     gethUrls = config.ethOptions.gethURLs;
@@ -14,18 +15,20 @@ let ethClients = [], // reference to clients
 function resubscribeIsRequired(client_id) {
 
     if (client_id < ethClients.length && client_id >= 0) {
-        let ethClient = ethClients[client_id];
-        if (!ethClient.subscribe) {
-            return true;
-        } else {
-            if (!ethClient.subscribe.id) {
-                return true;
+        if (ethClients[client_id].subscribe && ethClients[client_id].provider) {
+            if (ethClients[client_id].subscribe.id) {
+                console.log("delta = ", lastBlock - ethClients[client_id].lastBlock);
+                return (lastBlock - ethClients[client_id].lastBlock > config.ethOptions.maxNodesDelta);
             } else {
-                return (lastBlock - ethClient.lastBlock > config.ethOptions.maxNodesDelta);
+                console.log("subscribe ID is NULL - resubscribe required");
+                return true;
             }
+        } else {
+            console.log("not subscribe or provider object - resubscribe required");
+            return true;
         }
-
     } else {
+        console.log("Out of index range - no resubscribe");
         return false
     }
 }
@@ -47,6 +50,43 @@ function getBestProvider() {
     }
 }
 
+
+function subscribe(client_id){
+    if (client_id < ethClients.length && client_id >= 0) {
+        ethClients[client_id].subscribe = ethClients[client_id].provider.eth.subscribe('newBlockHeaders', function (e, data) {
+            if (e)console.log(`${c.red}Failed connect to subscribe ${client_id}: ${c.white}${e}`);
+        }).on("data", function(blockHeader){
+            console.log(`${c.green}New block ${client_id}: ${c.white}${blockHeader.number}`);
+            ethClients[client_id].lastBlock = blockHeader.number;
+            if (ethClients[client_id].lastBlock > lastBlock) lastBlock = ethClients[client_id].lastBlock;
+        });
+    }
+}
+
+function resubscribe(){
+    for (let i=0; i<ethClients.length; i+=1){
+        if (resubscribeIsRequired(i)){
+
+            ethClients[i].provider = new Web3(new Web3.providers.WebsocketProvider(ethClients[i].url), 2000);
+
+            setTimeout(function () {
+                if (ethClients[i].subscribe){
+                    ethClients[i].subscribe.unsubscribe(function(error, success){
+                        if(success) {
+                            console.log('Successfully unsubscribed!');
+                            subscribe(i)
+                        }
+                    });
+                } else {
+                    subscribe(i)
+                }
+            }, 2100);
+        } else {
+            console.log(`${c.green}Subscription is fine #${i}`);
+        }
+    }
+}
+
 let initEthProviders = ()=>{
     for (let i=0; i<gethUrls.length; i+=1){
         ethClients.push({
@@ -58,44 +98,25 @@ let initEthProviders = ()=>{
         });
     }
 
-    for (let i=0; i<ethClients.length; i+=1){
-        ethClients[i].provider = new Web3(new Web3.providers.WebsocketProvider(ethClients[i].url));
-    }
+    resubscribe();
+    setInterval(resubscribe, 10000);
 
-    setInterval(function () {
-        for (let i=0; i<ethClients.length; i+=1){
-            if (ethClients[i].provider) {
-                if (resubscribeIsRequired(i)){
-
-                    if (ethClients[i].subscribe){
-                        ethClients[i].subscribe.unsubscribe(function(error, success){
-                            if(success) console.log('Successfully unsubscribed!');
-                        });
-                    }
-
-                    ethClients[i].subscribe = ethClients[i].provider.eth.subscribe('newBlockHeaders', function (e, data) {
-                        if (e) {
-                            console.log(`${c.red}Failed connect to subscribe ${ethClients[i].id}: ${c.white}${e}`);
-                            ethClients[i].subscribe_id = this.id;
-                        }
-                    }).on("data", function(blockHeader){
-                        console.log(`${c.green}New block ${ethClients[i].id}: ${c.white}${blockHeader.number}`);
-                        ethClients[i].lastBlock = blockHeader.number;
-                        if (ethClients[i].lastBlock > lastBlock) lastBlock = ethClients[i].lastBlock;
-                    });
-                } else {
-                    console.log(`${c.green}Subscription is fine #${i}`);
-                }
-            } else {
-                console.log(`${c.red}Provider is not defined #${i}`);
-            }
-        }
-
-    }, 20000)
 };
 
+const getLastBlocks = async (req, res) =>{
+    console.log(ethClients.length);
+    let tmpBlocks = [];
+    console.log(ethClients);
+    for (let i=0; i<ethClients.length; i+=1){
+        tmpBlocks.push(ethClients[i].lastBlock);
+        console.log(ethClients[i].lastBlock)
+    }
+    res.json({"nodes": tmpBlocks})
+
+};
 
 module.exports = {
     initEthProviders:  initEthProviders, // get DB instance
     getBestProvider: getBestProvider,
+    getLastBlocks: getLastBlocks,
 };
