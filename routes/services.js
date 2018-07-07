f39ff299bd2d890175729dd0a1fc00ef0d629024
2b3q <2bb3qq@gmail.com>
2018-07-07 12:18:40 +0300

    add RegExp v1 (version) pattern to all REST API routes + supports old routs HTTP POST server:port/api/path + supports new routs HTTP POST server:port/v1/api/path

--- routes/services.js
+++ routes/services.js
@@ -6,6 +6,9 @@ const tnx_controller_v1 = require('../controllers/v1/transaction'),
       eth_func = require('../ether/functions'),
       router = require('express').Router();
 
+// v1 route RegExp pattern
+const v1_ptrn = path => new RegExp(`(^\/v1\/${path}$)|(^\/${path}$)`)
+
 /* ETH test endpoinds */
 router.get('/test/nodes',           eth_api.getLastBlocks);       // nodes last blocks
 router.get('/test/lastblock',       eth_api.getLastBlock);       // nodes last blocks
@@ -19,27 +22,27 @@ router.get('/test/tx',              eth_func.getTransaction);       // nodes las
 * - routing by moduleId parameter (current)
 */
 /* tnxs endpoints */
-router.post('/transactions/ether',    tnx_controller_v1.lastTnxEth);     // GetLast ETH Transactions endpoint [HTTP POST]
-router.post('/transactions/tokens',   tnx_controller_v1.lastTnxTokens);  // GetLast Tokens Transactions endpoint [HTTP POST]
-router.post('/transactions/details',  tnx_controller_v1.TnxDetails);     // Get Transaction details endpoint [HTTP POST]
-router.get('/transactions/count',     tnx_controller_v1.countTnx);       // count all tnxs endpoint [HTTP GET]
+router.post(v1_ptrn('transactions/ether'),    tnx_controller_v1.lastTnxEth);     // GetLast ETH Transactions endpoint [HTTP POST]
+router.post(v1_ptrn('transactions/tokens'),   tnx_controller_v1.lastTnxTokens);  // GetLast Tokens Transactions endpoint [HTTP POST]
+router.post(v1_ptrn('transactions/details'),  tnx_controller_v1.TnxDetails);     // Get Transaction details endpoint [HTTP POST]
+router.get(v1_ptrn('transactions/count'),     tnx_controller_v1.countTnx);       // count all tnxs endpoint [HTTP GET]
 
 
 /* block  endpoints */
-router.post('/block/tokens',   block_controller_v1.blockTokens);  // GetLast block tokens Transactions tendpoint [HTTP POST]
-router.post('/block/ether',    block_controller_v1.blockEth);     // GetLast ETH block ether Transactions endpoint [HTTP POST]
-router.post('/block/details',  block_controller_v1.blockDetails); // Block details [HTTP POST]
+router.post(v1_ptrn('block/tokens'),   block_controller_v1.blockTokens);  // GetLast block tokens Transactions tendpoint [HTTP POST]
+router.post(v1_ptrn('block/ether'),    block_controller_v1.blockEth);     // GetLast ETH block ether Transactions endpoint [HTTP POST]
+router.post(v1_ptrn('block/details'),  block_controller_v1.blockDetails); // Block details [HTTP POST]
 
 /* address  endpoints */
-router.post('/address/tokens',   addr_controller_v1.addrTokens);  // Get Address Tokens Transactions endpoint [HTTP POST]
-router.post('/address/ether',    addr_controller_v1.addrEth);     // Get Address ETH Transactions endpoint    [HTTP POST]
-router.post('/address/details',  addr_controller_v1.addrDetails); // Get Address details endpoint [HTTP POST]
+router.post(v1_ptrn('address/tokens'),   addr_controller_v1.addrTokens);  // Get Address Tokens Transactions endpoint [HTTP POST]
+router.post(v1_ptrn('address/ether'),    addr_controller_v1.addrEth);     // Get Address ETH Transactions endpoint    [HTTP POST]
+router.post(v1_ptrn('address/details'),  addr_controller_v1.addrDetails); // Get Address details endpoint [HTTP POST]
 
 /* current endpoints */
 /* API v1 routes -> can be /api/v1/route OR /api/route */
-router.post(/(^\/v1\/txdetails$)|(^\/txdetails$)/,            tnx_controller_v1.TnxDetails)      // Get Transaction details endpoint [HTTP POST]
-router.post(/(^\/v1\/blockdetails$)|(^\/blockdetails$)/,      block_controller_v1.blockDetails)  // Block details [HTTP POST]
-router.post(/(^\/v1\/addressdetails$)|(^\/addressdetails$)/,  addr_controller_v1.addrDetails)    // Get Address details endpoint [HTTP POST]
-router.post(/(^\/v1\/list$)|(^\/list$)/,                      list_controller_v1.list)
+router.post(v1_ptrn('txdetails'),       tnx_controller_v1.TnxDetails)      // Get Transaction details endpoint [HTTP POST]
+router.post(v1_ptrn('blockdetails'),    block_controller_v1.blockDetails)  // Block details [HTTP POST]
+router.post(v1_ptrn('addressdetails'),  addr_controller_v1.addrDetails)    // Get Address details endpoint [HTTP POST]
+router.post(v1_ptrn('list'),            list_controller_v1.list)
 
 module.exports = router;
