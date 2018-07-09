afba4ceb4c4bd42680aa686d82ea7c2e8efe79c2
lriman <knight.max.jd@gmail.com>
2018-07-09 16:45:47 +0300

    token balance (close)

--- models/address.js
+++ models/address.js
@@ -64,19 +64,19 @@ const GetAddrTokenBalance = async options => {
   console.log(`erc20_cache col: ${config.store.cols.erc20_cache}`);
 
   let cachedTokenBlocks = async () => {
-      for (let i = 0; i < 5; i++) {
-          let tokenCacheCol_p = await dbquery.find(config.store.cols.erc20_cache, cache_selector);
-          if(tokenCacheCol_p.rows === 0) await wait(50);
-          else {
-              tokenCacheCol_p.forEach(c_block => {
-                  console.log(`c_block.lastblock = ${c_block.lastblock}`);
-                  if(c_block.lastblock > lastCachedBlock) lastCachedBlock = c_block.lastblock;
-                  return 0
-              });
-              break
-          }
+    for (let i = 0; i < 5; i++) {
+      let tokenCacheCol_p = await dbquery.find(config.store.cols.erc20_cache, cache_selector);
+      if(tokenCacheCol_p.rows === 0) await wait(50);
+      else {
+        tokenCacheCol_p.forEach(c_block => {
+          console.log(`c_block.lastblock = ${c_block.lastblock}`);
+          if(c_block.lastblock > lastCachedBlock) lastCachedBlock = c_block.lastblock;
+          return 0
+        });
+        break
       }
-      return 0 // done
+    }
+    return 0 // done
   };
 
   // DEBUG: lastCachedBlock
@@ -86,28 +86,28 @@ const GetAddrTokenBalance = async options => {
 
   let ctl_p = await dbquery.find(config.store.cols.erc20_cache, {'addr': addr, 'lastblock': 0});
   if(Array.isArray(ctl_p)) {
-      console.log('---------------- cache_tokens_selector find query ----------------');
-      console.log(ctl_p);
-      ctl_p.forEach(tkn => {
-          allTokensMap[tkn.tokenaddr] = {
-              addr: tkn.tokenaddr,
-              name: tkn.tokenname,
-              smbl: tkn.tokensmbl,
-              dcm:  tkn.tokendcm,
-              type: 20,
-              balance: tkn.value,
-              icon: '/api/token/icon/'+tkn.tokenaddr,
-              dynamic: 0
-          };
-      });
+    console.log('---------------- cache_tokens_selector find query ----------------');
+    console.log(ctl_p);
+    ctl_p.forEach(tkn => {
+      allTokensMap[tkn.tokenaddr] = {
+        addr: tkn.tokenaddr,
+        name: tkn.tokenname,
+        smbl: tkn.tokensmbl,
+        dcm:  tkn.tokendcm,
+        type: 20,
+        balance: tkn.value,
+        icon: '/api/token/icon/'+tkn.tokenaddr,
+        dynamic: 0
+      };
+    });
   }
 
 
   let last_tokens_selector = {
-      $or:[ { 'addrto':   addr },
-          { 'addrfrom': addr } ],
-      'block': { $gt: lastCachedBlock },
-      'tokentype': 20
+    $or:[ { 'addrto':   addr },
+      { 'addrfrom': addr } ],
+    'block': { $gt: lastCachedBlock },
+    'tokentype': 20
   };
   let lastTokens = await dbquery.distinct(cfg.store.cols.token, last_tokens_selector, 'tokenaddr');
 
@@ -116,79 +116,51 @@ const GetAddrTokenBalance = async options => {
 
   let lastTokensPromiseList = [];
   lastTokens.forEach(t => {
-      lastTokensPromiseList.push(dbquery.findOne(cfg.store.cols.token_head, { 'addr': t }));
+    lastTokensPromiseList.push(dbquery.findOne(cfg.store.cols.token_head, { 'addr': t }));
   });
 
   console.log('lastTokensPromiseList = ', lastTokensPromiseList);
 
   await Promise.all(lastTokensPromiseList)
-      .then((lastTokensPromiseList) => {
-          lastTokensPromiseList.forEach(tkn => {
-              if (tkn) {
-                  tkn.balance = '*';
-                  tkn.icon = "/api/token/icon/" + tkn.addr;
-                  tkn.dynamic = 0;
-                  allTokensMap[tkn.addr] = tkn;
-              }
-          });
+    .then((lastTokensPromiseList) => {
+      lastTokensPromiseList.forEach(tkn => {
+        if (tkn) {
+          tkn.balance = '*';
+          tkn.icon = "/api/token/icon/" + tkn.addr;
+          tkn.dynamic = 0;
+          allTokensMap[tkn.addr] = tkn;
+        }
       });
+    });
 
   console.log('---------------- allTokensMap ----------------');
   console.log(allTokensMap);
 
-  /*
-    for _, a := range lastTokens {
-		var tknHead structs.TokenHeader
-		err := tokenCol.Find(bson.M{"addr": a}).One(&tknHead)
-		if err == nil {
-			tknHead.Icon = "/api/token/icon/" + tknHead.Addr
-			tknHead.Dynamic = 0
-			tknHead.Balance = "*"
-			lastTokensMap[tknHead.Addr] = tknHead
-			allTokensMap[tknHead.Addr] = tknHead
-		}
-	}
+  let allTokens = allTokensMap.entries();
+  allTokens.sort(function(a,b){return a[1].name > b[1].name});
 
-	for _, a := range allTokensMap {
-		allTokens = append(allTokens, a)
-	}
+  let totalTokens = allTokens.length;
 
-	sort.Slice(allTokens, func(i, j int) bool {return allTokens[i].Addr < allTokens[j].Addr})
-	totalTokens := len(allTokens)
+  let fromI = skip;
+  let toI = skip + size;
+  if (fromI < 0) fromI = 0;
+  if (fromI > totalTokens) fromI = totalTokens;
+  if (toI < 0) toI = 0;
+  if (toI > totalTokens) toI = totalTokens;
+  if (fromI > toI) toI = fromI;
 
-	fromI := skip
-	toI := skip + size
-	if fromI < 0 {
-		fromI = 0
-	}
-	if fromI > totalTokens {
-		fromI = totalTokens
-	}
-	if toI < 0 {
-		toI = 0
-	}
-	if toI > totalTokens {
-		toI = totalTokens
-	}
-	if fromI > toI {
-		toI = fromI
-	}
+  let partToken = [];
+  for (let i=fromI; i<toI; i+=1){
+    partToken.push(allTokens[i])
+  }
 
-	part := allTokens[fromI:toI]
+  partToken.forEach(t => {
+    let balance = eth_func.providerEthProxy('tokenbalance', {walletAddr: addr, tokenAddr: t.addr})
+    console.log("balance = ", balance);
+    t.balance = balance;
+  });
 
-	for i := range part{
-		if part[i].Balance == "*" {
-			balance, err := tokenBalance(clearAddr, part[i].Addr)
-			if err == nil {
-				part[i].Balance = balance
-			} else {
-				part[i].Balance = ""
-			}
-		}
-	}
-
-	return part, len(allTokens)*/
-
+  return {tokens: partToken, total: totalTokens}
 };
 /*
 * Get address tnx:
