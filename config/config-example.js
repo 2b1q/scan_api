3eb8352aa7f25a1a404d8d5c009277f2900422ec
lriman <knight.max.jd@gmail.com>
2018-07-16 11:15:00 +0300

    conf

--- config/config-example.js
+++ config/config-example.js
@@ -1,8 +1,8 @@
 const config = {};
 
 config.server = {
-    port: '3000',
-    ip: (process.env.NODE_ENV == 'PROD') ? '95.213.165.61' : '192.168.1.87' // hardcoded
+  port: '3000',
+  ip: (process.env.NODE_ENV == 'PROD') ? '95.213.165.61' : '192.168.1.87' // hardcoded
 };
 
 // cluster config workers (not by CPU count!)
@@ -10,84 +10,85 @@ config.workers = (process.env.NODE_ENV == 'PROD') ? 4 : 2;
 
 // DataStore config
 config.store = {
-    mongo: {
-        uri: 'mongodb://localhost:27017/', // hardcoded
-        dbname: 'bkx_testnet',
-        options: {
-            // autoIndex: false,
-            poolSize: 10 // количество подключений в пуле
-        },
-        max_skip: 300000 // max skip size limit
+  mongo: {
+    uri: 'mongodb://localhost:27017/', // hardcoded
+    dbname: 'bkx_testnet',
+    options: {
+      // autoIndex: false,
+      poolSize: 10 // количество подключений в пуле
     },
-    cols: {
-        eth:        'ether_txn',
-        token:      'token_txn',
-        block:      'block_header',
-        token_head: 'token_header',
-        contract:   'contract_header',
-        erc20_cache: 'erc20_cache',
-    }
+    max_skip: 300000 // max skip size limit
+  },
+  cols: {
+    eth:        'ether_txn',
+    token:      'token_txn',
+    block:      'block_header',
+    token_head: 'token_header',
+    contract:   'contract_header',
+    erc20_cache: 'erc20_cache',
+  }
 };
 
 // pager
 config.page = {
-    min_page: 1,
-    max_page: 30000,
-    min_size: 10,
-    max_size: 200
+  min_page: 1,
+  max_page: 30000,
+  min_size: 10,
+  max_size: 200
 };
 
 // modules
 config.modules = {
-    tnx:    'transactions',
-    block:  'block',
-    addr:   'address',
-    token:  'tokens'
+  tnx:    'transactions',
+  block:  'block',
+  addr:   'address',
+  token:  'tokens'
 };
 
 // tx types list
 config.list_type = {
-    eth:    'listOfETH',
-    token:  'listOfTokens'
+  eth:    'listOfETH',
+  token:  'listOfTokens',
+  token_balance:    'listOfTokenBalance',
 };
 
 // socket io client evens
 config.events = {
-    client: {
-        list:    'list',
-        tx_d:    'txDetails',
-        block_d: 'blockDetails',
-        addr_d:  'addressDetails'
-    }
+  client: {
+    list:    'list',
+    tx_d:    'txDetails',
+    block_d: 'blockDetails',
+    addr_d:  'addressDetails'
+  }
 };
 
 // REST API options
 config.restOptions = {
-    context: '/api',
-    logger:{ file: 'restapi.log', level: 'error' },
-    apiKeys: [ '11111-1111-222-3333', 'q1w2e3r4', 'B@NKEX','t0kEn' ], // hardcoded
-    domain: require('domain').create()
+  context: '/api',
+  logger:{ file: 'restapi.log', level: 'error' },
+  apiKeys: [ '11111-1111-222-3333', 'q1w2e3r4', 'B@NKEX','t0kEn' ], // hardcoded
+  domain: require('domain').create()
 };
 
 config.ethOptions = {
-    gethURLs: ["ws://94.130.171.164:8556"],
-    maxNodesDelta: 20,
-    upNodeFrequency: 10000,
-    get_provider_retries: 5,
-    get_provider_wait: 50,
+  gethURLs: ["ws://94.130.171.164:8556"],
+  maxNodesDelta: 20,
+  upNodeFrequency: 10000,
+  get_provider_retries: 5,
+  get_provider_wait: 50,
 
 };
 
 // colorize console
 config.color = {
-    yellow: "\x1b[33m",
-    blue: "\x1b[34m",
-    cyan: "\x1b[36m",
-    green: "\x1b[32m",
-    black: "\x1b[30m",
-    red: "\x1b[31m",
-    magenta: "\x1b[35m",
-    white: "\x1b[37m"
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  cyan: "\x1b[36m",
+  green: "\x1b[32m",
+  black: "\x1b[30m",
+  red: "\x1b[31m",
+  magenta: "\x1b[35m",
+  white: "\x1b[37m"
 };
 
 module.exports = config;
