3c710289becb3d0c0a9722cc3b846cfea445f050
2b3q <2bb3qq@gmail.com>
2018-09-21 09:59:30 +0300

    api 2.1 tests

---
+++ tests/api_2.1/test/block_details_REST_2.1.js
@@ -0,0 +1,30 @@
+const chai = require('chai'),
+    should = chai.should(),
+    expect = chai.expect,
+    supertest = require('supertest');
+
+const url = process.env.scanurl || 'http://localhost:3000';
+const api = supertest(url);
+
+describe('REST API v. 2.1 "block details"', () => {
+    let endpoint = '/api/v2_1/block/details';
+    let block = process.env.block || '6366555';
+    let block_head_keys = ['block', 'gasLimit', 'gasUsed', 'hash', 'innerTxCount', 'mainTxCount', 'time', 'tokenTxCount'];
+
+    it(`${endpoint}?block=${block} => should return "Content-Type: json" and 200 response`, (done) => {
+        api.get(`${endpoint}?block=${block}`)
+            .expect('Content-Type', /json/)
+            .expect(200, done());
+    });
+
+    it(`${endpoint}?block=${block} => should return a JSON with head property with fields ${block_head_keys} that is not empty`, (done) => {
+        api.get(`${endpoint}?block=${block}`)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body.head).to.have.all.keys(block_head_keys).that.is.not.empty;
+                done();
+            });
+    });
+});
