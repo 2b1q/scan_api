42e79b81468bcc013a09a2a18fd51771b15a9012
2b3q <2bb3qq@gmail.com>
2018-09-24 13:39:03 +0300

    api 2.1 tests

---
+++ tests/api_2.1/test/io_ERC20_token_txs.js
@@ -0,0 +1,48 @@
+const io = require('socket.io-client'),
+    chai = require('chai'),
+    expect = chai.expect;
+
+const url = process.env.scanurl || 'http://192.168.1.87:3001';
+const io_options = {
+    upgrade: false,
+    path: '/ws/v2_1',
+    transports: ['websocket'],
+};
+
+describe('Socket.io API v. 2.1 "ERC20 token transactions"', () => {
+    let addr = '0x763186eb8d4856d536ed4478302971214febc6a9';
+    let event = 'list';
+    let payload = {
+        listId: 'listOfTokens',
+        moduleId: 'erc20Token',
+        params: {
+            entityId: addr,
+            size: 20,
+            offset: 0,
+        },
+    };
+
+    const error_handler = (socket, error, callback) => {
+        socket.disconnect();
+        return callback(Error(error.errorMessage));
+    };
+
+    it(`ERC20 token transactions for "${addr}" => should return token transactions`, (done) => {
+        const socket = io.connect(
+            url,
+            io_options
+        );
+        socket.on('connect', () => {
+            socket.emit(event, JSON.stringify(payload), (e) => error_handler(socket, e, done));
+            socket.on(event, (data) => {
+                let object = JSON.parse(data);
+                expect(object)
+                    .to.have.property('head')
+                    .that.is.not.empty.and.to.have.property('rows');
+                done();
+                console.log(data);
+                socket.disconnect();
+            });
+        });
+    });
+});
