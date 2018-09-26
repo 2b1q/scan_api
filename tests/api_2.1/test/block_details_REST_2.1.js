55cc3e31b2a0d41fffe7c6d43d0d089b7af74231
2b3q <2bb3qq@gmail.com>
2018-09-26 17:27:42 +0300

    api 2.1 tests

--- tests/api_2.1/test/block_details_REST_2.1.js
+++ tests/api_2.1/test/block_details_REST_2.1.js
@@ -11,6 +11,10 @@ describe('REST API v. 2.1 "block details"', () => {
     let block = process.env.block || '6366555';
     let block_head_keys = ['block', 'gasLimit', 'gasUsed', 'hash', 'innerTxCount', 'mainTxCount', 'time', 'tokenTxCount'];
     let time = 'time';
+    let wrong_block = 100;
+    const HTTP404 = 404;
+    const HTTP400 = 400;
+    const ERRORCODE404 = HTTP404;
 
     it(`${endpoint}?block=${block} => should return "Content-Type: json" and 200 response`, (done) => {
         api.get(`${endpoint}?block=${block}`)
@@ -25,6 +29,27 @@ describe('REST API v. 2.1 "block details"', () => {
             .end((err, res) => {
                 if (err) return done(err);
                 expect(res.body.head.block).to.equal(Number(block));
+                done();
+            });
+    });
+
+    it(`${endpoint}?block=${block} => should return block details for block ${block}`, (done) => {
+        api.get(`${endpoint}?block=${block}`)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body.head.block).to.equal(Number(block));
+                done();
+            });
+    });
+
+    it(`${endpoint}?block=${wrong_block} => should return HTTP status code "${HTTP404}" and errorCode "${ERRORCODE404}"`, (done) => {
+        api.get(`${endpoint}?block=${wrong_block}`)
+            .expect(HTTP404)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body.errorCode).to.equal(ERRORCODE404);
                 done();
             });
     });
