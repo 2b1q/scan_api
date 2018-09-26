e35af88c9fb844f2920a2a216e6f6576a6e67ef3
2b3q <2bb3qq@gmail.com>
2018-09-26 14:12:28 +0300

    api 2.1 tests

--- tests/api_2.1/test/address_details_REST_2.1.js
+++ tests/api_2.1/test/address_details_REST_2.1.js
@@ -1,7 +1,6 @@
 const chai = require('chai'),
     expect = chai.expect,
-    supertest = require('supertest'),
-    check = require('../../utils/tools').check;
+    supertest = require('supertest');
 
 const url = process.env.scanurl || 'http://localhost:3000';
 const api = supertest(url);
@@ -11,7 +10,6 @@ describe('REST API v. 2.1 "address details"', () => {
     let addr = process.env.addr || '0x0000000000000000000000000000000000000000';
     let addr_head_keys = ['addr', 'mainTxCount', 'innerTxCount', 'tokenTxCount', 'tokenTxCount', 'balance'];
     let balance_keys = ['dcm', 'val'];
-    let time = 'updateTime';
 
     it(`${endpoint}?addr=${addr} => should return "Content-Type: json" and 200 response`, (done) => {
         api.get(`${endpoint}?block=${addr}`)
@@ -27,19 +25,6 @@ describe('REST API v. 2.1 "address details"', () => {
                 if (err) return done(err);
                 expect(res.body.head).to.have.all.keys(addr_head_keys).that.is.not.empty;
                 expect(res.body.head.balance).to.have.all.keys(balance_keys);
-                done();
-            });
-    });
-
-    it(`head => should have property '${time}' with UTC format time value`, (done) => {
-        api.get(`${endpoint}?addr=${addr}`)
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
-                expect(res.body.head)
-                    .to.have.property('time')
-                    .that.satisfy((time) => check.utc(time));
                 done();
             });
     });
--- tests/api_2.1/test/block_details_REST_2.1.js
+++ tests/api_2.1/test/block_details_REST_2.1.js
@@ -1,6 +1,7 @@
 const chai = require('chai'),
     expect = chai.expect,
-    supertest = require('supertest');
+    supertest = require('supertest'),
+    check = require('../../utils/tools').check;
 
 const url = process.env.scanurl || 'http://localhost:3000';
 const api = supertest(url);
@@ -9,6 +10,7 @@ describe('REST API v. 2.1 "block details"', () => {
     let endpoint = '/api/v2_1/block/details';
     let block = process.env.block || '6366555';
     let block_head_keys = ['block', 'gasLimit', 'gasUsed', 'hash', 'innerTxCount', 'mainTxCount', 'time', 'tokenTxCount'];
+    let time = 'time';
 
     it(`${endpoint}?block=${block} => should return "Content-Type: json" and 200 response`, (done) => {
         api.get(`${endpoint}?block=${block}`)
@@ -23,6 +25,19 @@ describe('REST API v. 2.1 "block details"', () => {
             .end((err, res) => {
                 if (err) return done(err);
                 expect(res.body.head).to.have.all.keys(block_head_keys).that.is.not.empty;
+                done();
+            });
+    });
+
+    it(`head => should have property '${time}' with UTC format time value`, (done) => {
+        api.get(`${endpoint}?block=${block}`)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body.head)
+                    .to.have.property(time)
+                    .that.satisfy((time) => check.utc(time));
                 done();
             });
     });
