e74d0f954b490f9aaa2b9490c75bca6c14cf9af0
2b3q <2bb3qq@gmail.com>
2018-09-27 17:42:57 +0300

    api 2.1 tests

--- tests/api_2.1/test/io_search_token_or_block.js
+++ tests/api_2.1/test/io_search_token_or_block.js
@@ -11,11 +11,12 @@ const io_options = {
 
 describe('Socket.io API v. 2.1 "search block or tokens"', () => {
     let serch_query = {
-        q: '123',
+        q: '2018',
         params: {
             size: 5,
         },
     };
+    let token = 'bKx';
     let event = 'search';
 
     const error_handler = (socket, error, callback) => {
@@ -49,18 +50,26 @@ describe('Socket.io API v. 2.1 "search block or tokens"', () => {
                 done();
                 console.log(data);
                 socket.disconnect();
-                serch_query.q = 'bKx';
             });
         });
     });
 
-    it(`event "${event}" and q:"${serch_query.q}" => server should emit "${event}" event with BKX token`, (done) => {
+    it(`event "${event}" and q:"${token}" => server should emit "${event}" event with BKX token`, (done) => {
         const socket = io.connect(
             url,
             io_options
         );
         socket.on('connect', () => {
-            socket.emit(event, JSON.stringify(serch_query), (e) => error_handler(socket, e, done));
+            socket.emit(
+                event,
+                JSON.stringify({
+                    q: token,
+                    params: {
+                        size: 5,
+                    },
+                }),
+                (e) => error_handler(socket, e, done)
+            );
             socket.on(event, (data) => {
                 expect(JSON.parse(data).tokens[0].smbl).to.includes('BKX');
                 done();
